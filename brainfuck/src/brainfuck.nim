4cd0b341a2eca1711fc842b2396526a3b8fe6934
Alyxia Sother <lexisoth2005@gmail.com>
2021-06-28 22:25:20 +0200

    [brainfuck] Move the interpreter to a function

--- brainfuck/src/brainfuck.nim
+++ brainfuck/src/brainfuck.nim
@@ -1,13 +1,3 @@
-import os
-
-let code = if paramCount() > 0: readFile paramStr(1)
-else: readAll stdin
-
-var
-  tape = newSeq[char]()
-  codePos = 0
-  tapePos = 0
-
 # In Nim 0.11 and above, the `inc` and `dec` procs for `char`s have
 # under/overflow checks. This means that when we have the character `\0` and decrement it,
 # we end up with a runtime error! Instead, in brainfuck, we want to wrap around
@@ -20,28 +10,47 @@ proc xinc(c: var char) = inc c
 proc xdec(c: var char) = dec c
 {.pop.}
 
-proc run(skip = false): bool =
-  while tapePos >= 0 and codePos < code.len:
-    if tapePos >= tape.len:
-      tape.add '\0'
+proc interpret*(code: string) =
+  ## Interprets the Brainfuck `code` string, reading from stdin and writing to stdout.
+  var
+    tape = newSeq[char]()
+    codePos = 0
+    tapePos = 0
 
-    if code[codePos] ==  '[':
+  proc run(skip = false): bool =
+    while tapePos >= 0 and codePos < code.len:
+      if tapePos >= tape.len:
+        tape.add '\0'
+
+      if code[codePos] ==  '[':
+        inc codePos
+        let oldPos = codePos
+        while run(tape[tapePos] == '\0'):
+          codePos = oldPos
+
+      elif code[codePos] == ']':
+        return tape[tapePos] != '\0'
+
+      elif not skip:
+        case code[codePos]
+        of '+': xinc tape[tapePos]
+        of '-': xdec tape[tapePos]
+        of '>': inc tapePos
+        of '<': dec tapePos
+        of '.': stdout.write tape[tapePos]
+        of ',': tape[tapePos] = stdin.readChar
+        else: discard
+
       inc codePos
-      let oldPos = codePos
-      while run(tape[tapePos] == '\0'):
-        codePos = oldPos
-    elif code[codePos] == ']':
-      return tape[tapePos] != '\0'
-    elif not skip:
-      case code[codePos]
-      of '+': xinc tape[tapePos]
-      of '-': xdec tape[tapePos]
-      of '>': inc tapePos
-      of '<': dec tapePos
-      of '.': stdout.write tape[tapePos]
-      of ',': tape[tapePos] = stdin.readChar
-      else: discard
 
-    inc codePos
+  discard run()
 
-discard run()
+when isMainModule:
+  import os
+
+  echo "Welcome to Brainfuck"
+
+  let code = if paramCount() > 0: readFile paramStr(1)
+  else: readAll stdin
+
+  interpret code
