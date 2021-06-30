cd0e4889b23ae5eed3a521eecc2f8849f6669309
Alyxia Sother <lexisoth2005@gmail.com>
2021-06-30 21:17:54 +0200

    [AUR] Use types instead of raw JSON

--- AUR/src/aur.nim
+++ AUR/src/aur.nim
@@ -2,17 +2,23 @@ import httpclient, json, strutils
 
 var client = newHttpClient()
 
+type
+    Package = object
+        Name: string
+        Description: string
+
+type
+    Results = object
+        results: seq[Package]
+
 proc searchPkg*(query: string): void =
-    # TODO: https://nim-lang.org/docs/httpclient.html
-    #       https://nim-lang.org/docs/json.html
-    # Simple AURweb client
     var i = 0
     var res = client.getContent("https://aur.archlinux.org/rpc/?v=5&type=search&arg=" & query)
-    var jsonnode = parseJson(res)
-    for node in jsonnode["results"]:
+    var packages = to(parseJson(res), Results)
+    for pkg in packages.results:
         if i < 5:
-            echo replace($node["Name"], "\"", "")
-            echo "  ", replace($node["Description"], "\"", "")
+            echo pkg.Name
+            echo pkg.Description
             echo "\n"
             inc i
         else:
