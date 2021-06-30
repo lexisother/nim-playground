5a1e14c0d2885b7aaf0f28a8cd1db6cbad26346c
Alyxia Sother <lexisoth2005@gmail.com>
2021-06-30 14:27:06 +0200

    [AUR] Display first 5 results

--- AUR/src/aur.nim
+++ AUR/src/aur.nim
@@ -6,9 +6,15 @@ proc searchPkg*(query: string): void =
     # TODO: https://nim-lang.org/docs/httpclient.html
     #       https://nim-lang.org/docs/json.html
     # Simple AURweb client
+    var i = 0
     var res = client.getContent("https://aur.archlinux.org/rpc/?v=5&type=search&arg=" & query)
     var jsonnode = parseJson(res)
-    echo jsonnode
+    for node in jsonnode["results"]:
+        if i < 5:
+            echo node["Name"]
+            inc i
+        else:
+            return
     
 
 searchPkg("github")
